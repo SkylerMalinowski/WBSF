e7a8de17bbcca0ddc101c5fb2153ff300f5d1045
jonatanYanovsky <leapformula4@gmail.com>
2017-02-21 21:13:13 -0500

    storeStockInformation(stockSymbol, price, theDate, theTime)

--- db_1.py
+++ db_1.py
@@ -1,28 +1,49 @@
 '''
-Created by Jonatan Yanovsky on 2/19/2017
-database prototype 1 - serverless implementation using python and sqlite
+Created by Jonatan Yanovsky on 2/21/2017
+Database implementation using python and sqlite
+using code from: https://docs.python.org/3/library/sqlite3.html
+
+Note to other team members: calling fetchone() or fetchall() will only work
+once. You cannot use this statement twice on the same object as the object
+will be None-type after the first call.
 '''
+
 
 import sqlite3
 
-conn = sqlite3.connect('database.db')
-c = conn.cursor() # open the database file
+# function parameters: string, float (2 decimals), string, string
+def storeStockInformation(stockSymbol, price, theDate, theTime):
 
-''' run this line only once - when you create the database
-# creates a table with one column (id)
-c.execute("CREATE TABLE try (id INTEGER)")
-'''
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor() # open the database file
 
-# insert the value 10 into the database
-# running this command over and over along with commit()
-# increases the size of the database (adds more rows)
-c.execute("INSERT INTO try VALUES (10)")
+    stockSymbol1 = stockSymbol.lower() 
+    stockSymbol = (stockSymbol1,) # format: ('goog',)
 
-conn.commit() # save changes to the database
+    # try to find the stock symbol's table if it is already in the database
+    val1 = c.execute("SELECT name FROM sqlite_master WHERE type='table' AND name = ? ", stockSymbol)
 
-# read a column (id) from database
-val = c.execute("SELECT id FROM try")
+    try: # table already exists
+        val1.fetchone()[0] # if this works, then it doesn't jump to except
 
+    except: # table doesn't exist - create a new table
+        c.execute("CREATE TABLE " + stockSymbol1 + " (lastTradePrice FLOAT, date STRING, time STRING)")
 
-# print out all rows for that column that were in the database
-print(val.fetchall())
+    # insert data into the table
+    inputs = [(price),(theDate),(theTime)]
+    c.execute("INSERT INTO " + stockSymbol1 + " VALUES ( ?, ?, ?)", inputs) 
+
+    conn.commit() # save changes to the database
+
+    # for debugging:
+
+    # read table data from database
+    #val = c.execute("SELECT * FROM " + stockSymbol1 + "")
+
+    # print out all rows/columns for that table
+    #print("printing from " + stockSymbol1 + ": ")
+    #print(val.fetchall())
+    #print(" ")
+
+    conn.close() # close the database.db file
+    return
