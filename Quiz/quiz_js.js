498b50fb640d78de6633b3f5cfadcf310c391156
Skyler Malinowski <skyler.malinowski@gmail.com>
2017-03-08 21:11:45 -0500

    Update quiz_js.js

--- Quiz/quiz_js.js
+++ Quiz/quiz_js.js
@@ -1,24 +1,24 @@
 (function() {
   var questions = [{
-    question: "Q1?",
-    choices: [0, 0, 0, 0, 0],
-    correctAnswer: 0	// index of choices
+    question: "What is 2*5?",
+    choices: [2, 5, 10, 15, 20],
+    correctAnswer: 2	// index in choices[]
   }, {
-    question: "Q2?",
-    choices: [0, 0, 0, 0, 0],
+    question: "What is 3*6?",
+    choices: [3, 6, 9, 12, 18],
+    correctAnswer: 4
+  }, {
+    question: "What is 8*9?",
+    choices: [72, 99, 108, 134, 156],
     correctAnswer: 0
   }, {
-    question: "Q3?",
-    choices: [0, 0, 0, 0, 0],
-    correctAnswer: 0
+    question: "What is 1*7?",
+    choices: [4, 5, 6, 7, 8],
+    correctAnswer: 3
   }, {
-    question: "Q4?",
-    choices: [0, 0, 0, 0, 0],
-    correctAnswer: 0
-  }, {
-    question: "Q5?",
-    choices: [0, 0, 0, 0, 0],
-    correctAnswer: 0
+    question: "What is 8*8?",
+    choices: [20, 30, 40, 50, 64],
+    correctAnswer: 4
   }];
 
   var questionCounter = 0; //Tracks question number
@@ -161,5 +161,9 @@
         numCorrect++;
       }
     }
+
+    score.append('You got ' + numCorrect + ' questions out of ' +
+      questions.length + ' right!!!');
+    return score;
   }
 })();
