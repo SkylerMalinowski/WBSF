dc917f8651ccbf2a447c440a0591c7049dbc3099
Hitmonrocker <vinstert@comcast.net>
2017-03-26 21:30:35 -0400

    Add files via upload

--- TestPlot.py
+++ TestPlot.py
@@ -20,6 +20,11 @@ def fetchData(stockSymbol,startYear,endYear):
 def fetchDataToday(stockSymbol,startYear):
 	info=web.DataReader(stockSymbol,'yahoo',datetime(int(startYear),1,1),time.strftime("%d-%m-%Y"))
 	return info
+def totalTogether(stockSymbol,Webster):
+	fig=go.create_candlestick(Webster.Open,Webster.High,Webster.Low,Webster.Close,dates=Webster.index)
+	figure=obj.Scatter(y=Webster.High,x=Webster.index,line=dict(color=('rgb(0,0,0)')))
+	fig['data'].extend([figure])
+	py.plot(fig, filename=stockSymbol+'_Line',validate=False)
 def main():
 	var = input('Enter A Stock Symbol ')
 	timeBegin=input('Enter Start year ')
@@ -28,6 +33,7 @@ def main():
 	#totalData=fetchData(var,timeBegin,timeEnd) #uncomment these 3 lines to do a range of date and not to current (1)
 	makeCandleStickGraph(var,totalDataCurrent)
 	makeLineGraph(var,totalDataCurrent)
+	totalTogether(var,totalDataCurrent)
 	#makeCandleStickGraph(var,totalData) (2)
 	#makeLineGraph(var,totalData) (3)
 main() 
