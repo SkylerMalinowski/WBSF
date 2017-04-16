a3ff8dfdd4cdf610b41bee0dab8ee8b9981020bd
Gregory Leonberg <GLeonberg@users.noreply.github.com>
2017-04-15 22:27:49 -0400

    Add files via upload

--- 1_Code/userDB/helpers.js
+++ 1_Code/userDB/helpers.js
@@ -1,86 +1,86 @@
 function dereg(u, p) {
-				var req = new XMLHttpRequest();
-				
-				req.onload = function(){
-					return req.responseText;
-				}
-				
-				req.open("GET", "/userDB/remUser/?u=" + u + "&p=" + p, false);
-				req.send();
-				
-				return req.responseText;
-			}
+	var req = new XMLHttpRequest();
+	
+	req.onload = function(){
+		return req.responseText;
+	}
+	
+	req.open("GET", "/userDB/remUser/?u=" + u + "&p=" + p, false);
+	req.send();
+	
+	return req.responseText;
+}
 
 function reg(u, p) {
-				var req = new XMLHttpRequest();
-				
-				req.onload = function(){
-					return req.responseText;
-				}
-				
-				req.open("GET", "/userDB/addUser/?u=" + u + "&p=" + p, false);
-				req.send();
-				
-				return req.responseText;
-			}
+	var req = new XMLHttpRequest();
+	
+	req.onload = function(){
+		return req.responseText;
+	}
+	
+	req.open("GET", "/userDB/addUser/?u=" + u + "&p=" + p, false);
+	req.send();
+	
+	return req.responseText;
+}
 
 function print(p) {
-				var req = new XMLHttpRequest();
-				
-				req.onload = function(){
-					return req.responseText;
-				}
-				
-				req.open("GET", "/userDB/printTable/?p=" + p, false);
-				req.send();
-				
-				return req.responseText;
-			}
+	var req = new XMLHttpRequest();
+	
+	req.onload = function(){
+		return req.responseText;
+	}
+	
+	req.open("GET", "/userDB/printTable/?p=" + p, false);
+	req.send();
+	
+	return req.responseText;
+}
 
 function logout() {
-				var req = new XMLHttpRequest();
-				req.onload = function(){
-					return req.responseText;
-				}
-				
-				req.open("GET", "/userDB/logout/?s=" + window.name, false);
-				req.send();
-				
-				return req.responseText;
-			}
+	var req = new XMLHttpRequest();
+	req.onload = function(){
+		return req.responseText;
+	}
+	
+	req.open("GET", "/userDB/logout/?s=" + window.name, false);
+	req.send();
+	
+	return req.responseText;
+}
 
 function login(u, p, callback) {
-				var req = new XMLHttpRequest();
-				req.onload = function(){
-					return req.responseText;
-					window.name = req.responseText;
-				}
-				
-				req.open("GET", "/userDB/login/?u=" + u + "&p=" + p, false);
-				req.send();
-				
-				return req.responseText;
-			}
-			
+	var req = new XMLHttpRequest();
+	req.onload = function(){
+		return req.responseText;
+		window.name = req.responseText;
+	}
+	
+	req.open("GET", "/userDB/login/?u=" + u + "&p=" + p, false);
+	req.send();
+	
+	return req.responseText;
+}
+
 function price(s) {
-				var req = new XMLHttpRequest();
-				req.onload = function(){
-					return req.responseText;
-				}
-				
-				req.open("GET", "/ticker/?s=" + s, false);
-				req.send();
-				return req.responseText
-			}
-			
+	var req = new XMLHttpRequest();
+	req.onload = function(){
+		return req.responseText;
+	}
+	
+	req.open("GET", "/ticker/?s=" + s, false);
+	req.send();
+	return req.responseText
+}
+
 function news(s) {
-				var req = new XMLHttpRequest();
-				req.onload = function(){
-					return req.responseText;
-				}
-				
-				req.open("GET", "/news/?s=" + s, false);
-				req.send();
-				
-				return req.responseText;
-			}
+	var req = new XMLHttpRequest();
+	req.onload = function(){
+		return req.responseText;
+	}
+	
+	req.open("GET", "/news/?s=" + s, false);
+	req.send();
+	
+	return req.responseText;
+}
