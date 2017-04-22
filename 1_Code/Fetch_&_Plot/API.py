2f3019e7af46dbd3bead14a09e30a3518c95214e
Hitmonrocker <vinstert@comcast.net>
2017-04-22 17:18:04 -0400

    Add files via upload

--- 1_Code/Fetch_&_Plot/API.py
+++ 1_Code/Fetch_&_Plot/API.py
@@ -23,6 +23,35 @@ app = Flask(__name__)
 # main function, initialize web app with IP and port
 if __name__ == '__main__':
 	app.run(host = "127.0.0.1", port = 80, debug = False)
+	
+# stock ticker and news stuff
+def getCurrentPrice(Sym):
+	ticker = Share(Sym)
+	return ticker.get_price()
+
+def getPercentChange(Sym):
+	ticker = Share(Sym)
+	return ticker.get_percent_change()
+
+def getNewsTitle(feed):
+	return feed['feed']['title']
+
+def getNews(feed, n):
+	return feed['entries'][n]['title']
+
+@app.route('/ticker/')
+def ticker():
+	tick = request.args.get('s')
+	feed = feedparser.parse('http://finance.yahoo.com/rss/headline?s=%s' %tick)
+	ret = "The current price of " + tick + " is $" + getCurrentPrice(tick) + ". This is a " + getPercentChange(tick) + " change."
+	return ret
+
+@app.route('/news/')
+def news():
+	tick = request.args.get('s')
+	feed = feedparser.parse('http://finance.yahoo.com/rss/headline?s=%s' %tick)
+	ret = "The top headline for " + tick + " from " + getNewsTitle(feed) + " is: <br><br>" + getNews(feed, 0)
+	return ret
 
 # Api To Get Graph
 @app.route('/graph/')
@@ -38,15 +67,20 @@ def getGraph():
 			
 			Prediction_Data=Fetching.fetchDataSpec(var,(datetime.now()+timedelta(days=-45))) # Get the data from just the past month for the prediciton part
 			
-			Prediction_Data_Length=len(Prediction_Data.High) # Lenght of the predictin Data to save the recalc of it
+			Prediction_Data_Length=len(Prediction_Data.Close) # Lenght of the predictin Data to save the recalc of it
 
-			Coeffcients=LinearAlgebra.coeffcients_Generator(LinearAlgebra.makeXVals_Matrix(10,timeBegin,Prediction_Data_Length),LinearAlgebra.makeY_Matrix(Prediction_Data.High)) #coeffcients for prediction fucntion a0-a10
+			Coeffcients=LinearAlgebra.coeffcients_Generator(LinearAlgebra.makeXVals_Matrix(10,timeBegin,Prediction_Data_Length),LinearAlgebra.makeY_Matrix(Prediction_Data.Low)) #coeffcients for prediction fucntion a0-a10
 			
-			Prediction_Model=LinearAlgebra.makeOutY(Coeffcients,Prediction_Data_Length,timeBegin,totalDataCurrent.High,googData) # Gets Prediciton Model or scatter of predicted points these points are also normalized
+			Prediction_Model=LinearAlgebra.makeOutY(Coeffcients,Prediction_Data_Length,timeBegin,totalDataCurrent.Low,googData) # Gets Prediciton Model or scatter of predicted points these points are also normalized
+			
 			
 			pointY=LinearAlgebra.getPointY(Coeffcients,timeBegin,googData[0]['LastTradePrice'],totalDataCurrent.High[len(totalDataCurrent)-1]) #gets Predictiion Point for the next day independently so I can calculate individual days
 			
-			url=Graphing.totalTogether(var,totalDataCurrent,googData,Prediction_Model,pointY)
+			R=RSI.PredictRSI(totalDataCurrent.Close)
+
+			pointY=LinearAlgebra.getPointY(Coeffcients,timeBegin,googData[0]['LastTradePrice'],totalDataCurrent.Low[len(totalDataCurrent)-1]) #gets Predictiion Point for the next day independently so I can calculate individual days
+			
+			url=Graphing.totalTogether(var,totalDataCurrent,googData,Prediction_Model,pointY,R) #Print Final Graph with everything together
 			
 			return ("<iframe width="+'"'+"900"+'"' + " height="+'"'+"800"+'"'+ " frameborder="+'"'+"0"+'"'+ " scrolling="+'"'+"no"+'"'+" src=<"+url+"></iframe>")
 	pass
