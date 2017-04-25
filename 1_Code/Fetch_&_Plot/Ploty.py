f1586e82733a26c3bfe35aecc73c1cf1a5ee42e3
jonatanYanovsky <leapformula4@gmail.com>
2017-04-25 14:47:43 -0400

    Update Ploty.py

--- 1_Code/Fetch_&_Plot/Ploty.py
+++ 1_Code/Fetch_&_Plot/Ploty.py
@@ -1,4 +1,2 @@
 import plotly 
-#plotly.tools.set_credentials_file(username='GregL', api_key='GM3NsRZQmHLNFKZWLlre')
 plotly.tools.set_credentials_file(username='up2you', api_key='gHFvOGQzS7n2Y559LN3u')
-#C:\\cygwin\bin\Ploty.py
